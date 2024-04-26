69d6fb023102a657f3f044ff9d3a55b80afa23d4
mohammediche <mohammediche@gmail.com>
2024-04-26 18:34:17 +0200

    test: add a test when the file is valid (75% statements)

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -6,6 +6,7 @@ import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import { localStorageMock } from "../__mocks__/localStorage.js"
+import Store from "../__mocks__/store.js"
 
 
 describe("Given I am connected as an employee", () => {
@@ -37,6 +38,31 @@ describe("Given I am connected as an employee", () => {
       // expect expressions
       expect(handleChangeFile).toHaveBeenCalled()
       expect(inputFile.files[0].type).not.toMatch(/image\/(jpeg|jpg|png)/)
+    })
+    test('Then I put a correct file, it should store it', () =>{
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      document.body.innerHTML = NewBillUI()
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      const store = Store
+      const newBill = new NewBill({
+        document, onNavigate, store, localStorage: window.localStorage
+      })
+      const inputFile = screen.getByTestId('file')
+      const validFile = new File(["valid content"], "valid-file.jpg", {
+        type: "image/jpg",
+      });
+      const handleChangeFile = jest.fn((e) => newBill.handleChangeFile(e))
+      inputFile.addEventListener('change', handleChangeFile)
+      fireEvent.change(inputFile, { target: { files: [validFile] } });
+      expect(newBill.billId).not.toBe('null')
+      expect(newBill.fileUrl).not.toBe('null')
+      expect(newBill.fileName).not.toBe('null')
     })
   })
 })
